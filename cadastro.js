5f50750ffcd0a1cb214e0413e5b13b988f31557d
lucasxvirtual <lucas18_2006@hotmail.com>
2016-07-14 20:05:41 -0300

    Create cadastro.js
    script do modulo cadastro.html

---
+++ cadastro.js
@@ -0,0 +1,15 @@
+function cadastro(){
+	var nome = document.getElementById("idNome").value;
+	var sobrenome = document.getElementById("idSobrenome").value;
+	var email = document.getElementById("idEmail").value;
+	var password = document.getElementById("idPassword").value;
+	var repeatPassword = document.getElementById("idRepeatPassword").value;
+	var correctPassword;
+	if (password == repeatPassword){
+		correctPassword = true;
+	}
+
+	
+	
+	
+}
