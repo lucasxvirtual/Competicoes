61ee24c6a7fdbd9bac9a0ed958c206d174c26d6d
lucasxvirtual <lucas18_2006@hotmail.com>
2016-07-16 08:29:10 -0300

    Add files via upload

--- cadastro.js
+++ cadastro.js
@@ -1,15 +1,32 @@
 function cadastro(){
+	var myFirebaseRef = new Firebase("https://competicoes.firebaseio.com/");
 	var nome = document.getElementById("idNome").value;
-	var sobrenome = document.getElementById("idSobrenome").value;
-	var email = document.getElementById("idEmail").value;
-	var password = document.getElementById("idPassword").value;
-	var repeatPassword = document.getElementById("idRepeatPassword").value;
-	var correctPassword;
-	if (password == repeatPassword){
-		correctPassword = true;
+	var apelido = document.getElementById("idApelido").value;
+	var rg = document.getElementById("idRG").value;
+	var cpf = document.getElementById("idCPF").value;
+	var clube = document.getElementById("idClube").value;
+	var categoria = document.getElementById("idCategoria").value;
+	var nomeMae = document.getElementById("idNomeMae").value;
+	var nascimento = document.getElementById("idNascimento").value;
+	if(nome != "" && apelido != "" && rg != "" && cpf != "" && clube != "" && categoria != "" && nomeMae != "" && nascimento != ""){
+		myFirebaseRef.set({
+  		NOME: nome,
+  		APELIDO: apelido,
+			NASCIMENTO: nascimento,
+  		RG: rg,
+			CPF: cpf,
+			CLUBE: clube,
+			CATEGORIA: categoria,
+			MAE: nomeMae
+		});
 	}
 
-	
-	
-	
+}
+
+function cpf_incorreto(e){
+	myFirebaseRef.set({
+  	name: "inc",
+  	lastName: "inc",
+  	email: "ola"
+	});
 }
